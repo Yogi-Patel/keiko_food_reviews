d00d730f5bfa32eaf4241a9605e302cdb0fd0663
Yogi_Patel <yogiranchhod@gmail.com>
2023-10-15 02:54:00 -0700

    Added page stubs for the review list, review grid, photos, and review map locations.

---
+++ lib/pages/review_list/review_list.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ReviewList extends StatefulWidget {
+  const ReviewList({super.key});
+
+  //we are going to
+  static const String route = '/review_list';
+
+  @override
+  State<ReviewList> createState() => _ReviewListState();
+}
+
+class _ReviewListState extends State<ReviewList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
